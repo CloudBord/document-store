81515cbbcea2acbf0de65836c59c69b5da281c8c
Lex de Kort <2232771+lex-kort@users.noreply.github.com>
2024-06-21 18:40:15 +0200

    Add Dockerfile Add RabbitMQ NuGet

--- Document.Store/Functions/DeleteDocumentFunction.cs
+++ Document.Store/Functions/DeleteDocumentFunction.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
 namespace Document.Store.Functions
 {
     public class DeleteDocumentFunction
     {
+        private readonly ILogger _logger;
+
+        public DeleteDocumentFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<DeleteDocumentFunction>();
+        }
+
+        [Function("DeleteDocumentFunction")]
+        public void Run([RabbitMQTrigger("document-delete", ConnectionStringSetting = "")] string myQueueItem)
+        {
+            _logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
     }
 }
